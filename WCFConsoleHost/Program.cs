8c5e97bc17901f5a814b863b479952b03dfde682
konakallasamuel <konakallasamuel@KONAKALLASAMUEL>
2021-08-24 18:53:22 +0530

    Add project files.

---
+++ WCFConsoleHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(ClassLibrary1.HelloSVC)))
+            {
+                host.Open();
+                Console.WriteLine("Host started at:"+DateTime.Now.ToShortTimeString());
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
