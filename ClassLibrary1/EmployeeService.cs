cdae83d0a7ebf2a7289e9691bebdd7a48283675b
Konakalla Samuel <konakalla.samuel@nagarro.com>
2021-08-27 11:36:16 +0530

    commiting all

---
+++ ClassLibrary1/EmployeeService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace ClassLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmployeeService" in both code and config file together.
+    public class EmployeeService : IEmployeeService
+    {
+        public Employee GetEmployee(int id)
+        {
+            Employee emp = new Employee();
+            string con = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection scon = new SqlConnection(con))
+            {
+                SqlCommand cmd = new SqlCommand("spGetEmployee", scon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", id);
+                scon.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    emp.Id =Convert.ToInt32( dr["Id"]);
+                    emp.Name = dr["Name"].ToString();
+                    emp.Gender = dr["Gender"].ToString();
+                    emp.DateOfBirth =Convert.ToDateTime( dr["DateOfBirth"]);
+                }
+            }
+
+            return emp;
+        }
+        public void SaveEmployee(Employee emp)
+        {
+            string con = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection scon = new SqlConnection(con))
+            {
+                SqlCommand cmd = new SqlCommand("spGetEmployee", scon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", emp.Id);
+                cmd.Parameters.AddWithValue("@Name", emp.Name);
+                cmd.Parameters.AddWithValue("@Gender", emp.Gender);
+                cmd.Parameters.AddWithValue("@DateOfBirth", emp.DateOfBirth);
+                scon.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+}
---
+++ ClassLibrary1/IEmployeeService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmployeeService" in both code and config file together.
+    [ServiceContract]
+    public interface IEmployeeService
+    {
+        [OperationContract]
+        Employee GetEmployee(int id);
+
+        void SaveEmployee(Employee emp);
+    }
+}
